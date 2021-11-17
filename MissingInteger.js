56c4022b1cebc217d32f1a939532c9ecc862cef9
Karen Okonkwo <karenokonkwo29@gmail.com>
2021-11-17 14:45:19 +0100

    Include shorter solution for MissingInteger

--- MissingInteger.js
+++ MissingInteger.js
@@ -2,32 +2,49 @@
 // console.log('this is a debug message');
 
 function solution(A) {
-    // write your code in JavaScript (Node.js 8.9.4)
-    
-    var ASorted = [];
-    var min = Math.min(...A);
-    var max = Math.max(...A);
-    var smlPosInt = undefined;
-    var ASorted = A.sort((a,b) => a - b);
-    var ASet = new Set();
-    ASorted.forEach(value => {
-      ASet.add(value);
-    })
-    
-    if (max < 0){
-       smlPosInt = 1;
-    } else {
-      for (let i = 1; i <= max; i++){
-            if (!ASet.has(i)) {
-              smlPosInt = i;
-              break;
-            }
-      }
-    }
-    
-    if (!smlPosInt){
-      smlPosInt = max + 1;
-    }
-    return smlPosInt;
-    
+	// write your code in JavaScript (Node.js 8.9.4)
+
+	var ASorted = [];
+	var min = Math.min(...A);
+	var max = Math.max(...A);
+	var smlPosInt = undefined;
+	var ASorted = A.sort((a, b) => a - b);
+	var ASet = new Set();
+	ASorted.forEach((value) => {
+		ASet.add(value);
+	});
+
+	if (max < 0) {
+		smlPosInt = 1;
+	} else {
+		for (let i = 1; i <= max; i++) {
+			if (!ASet.has(i)) {
+				smlPosInt = i;
+				break;
+			}
+		}
+	}
+
+	if (!smlPosInt) {
+		smlPosInt = max + 1;
+	}
+	return smlPosInt;
+}
+
+// Updated solution
+function solution2(A) {
+	A.sort((a, b) => a - b);
+	let expectedInt = 1;
+
+	for (let num of A) {
+		if (num < expectedInt) {
+			// do nothing
+		} else if (num === expectedInt) {
+			expectedInt++;
+		} else {
+			return expectedInt;
+		}
+	}
+
+	return expectedInt;
 }
